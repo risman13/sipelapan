d124c326c1f14d2d53c9ab13fcade1004ecee294
sutejoramadhan <ramadhansutejo94@gmail.com>
2016-10-11 01:39:07 +0700

    core masternya, blm selesai

---
+++ application/libraries/Template.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Codeigniter Libraries
+ * Template Load
+ * 
+ * @package 	Codeigniter
+ * @subpackage 	Libraries
+ * @category 	Libraries
+ * @author 		Ramadhan Sutejo <ramadhan.sutejo@outlook.com>
+ */
+
+class Template
+{
+
+	protected $ci;
+
+	public function __construct()
+	{
+        $this->ci =& get_instance();
+	}
+
+	public function frontend()
+	{
+		# code...
+	}	
+
+}
+
+/* End of file Template.php */
+/* Location: ./application/libraries/Template.php */
