7530d6553e12cbe544dc4884e3a5f78a8d2be3f9
sutejoramadhan <ramadhansutejo94@gmail.com>
2016-10-19 22:51:55 +0700

    perubahan config db

--- application/config/database.php
+++ application/config/database.php
@@ -75,6 +75,28 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
+	'hostname' => 'blog.binadarma.ac.id',
+	'username' => 'ramadhansutejo',
+	'password' => 'user@ramadhan123',
+	'database' => 'polda_sipelapan',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+/*$db['default'] = array(
+	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
@@ -93,4 +115,4 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
-);
+);*/
