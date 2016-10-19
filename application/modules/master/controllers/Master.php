047294155bbff1b43ca9cf88691f9dba53f3afeb
sutejoramadhan <ramadhansutejo94@gmail.com>
2016-10-19 07:14:10 +0700

    add module master, proses data pekerjaan | Ramadhan Sutejo

--- application/modules/master/controllers/Master.php
+++ application/modules/master/controllers/Master.php
@@ -6,12 +6,19 @@ class Master extends MY_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model('master_pekerjaan_model');
+		$this->load->helper('form');
+		$this->load->library('form_validation');
 		$this->_init();
 	}
 
 	private function _init()
 	{
 		$this->output->set_template('__backend');
+
+		$this->output->js('assets/js/plugins/tables/datatables/datatables.min.js');
+		$this->output->js('assets/js/plugins/forms/selects/select2.min.js');
+		$this->output->js('assets/js/plugins/notifications/sweet_alert.min.js');
 
 		$this->output->section('navbar', '__template/__backend/navbar');
 		$this->output->section('pageheader', '__template/__backend/pageheader');
@@ -23,11 +30,41 @@ class Master extends MY_Controller {
  		$this->load->view('data_pekerjaan_content');
 	}
 
+	/* begin master data pekerjaan */
 	public function data_pekerjaan()
 	{
-		$this->load->view('data_pekerjaan_content');
+		$data = array(
+			'data_pekerjaan' => $this->master_pekerjaan_model->select_master_pekerjaan(), 
+		);
+
+		$this->load->view('data_pekerjaan_content', $data, FALSE);
 	}
 
+	public function data_pekerjaan_tambah()
+	{
+		$this->form_validation->set_rules('nama_pekerjaan', 'Nama Pekerjaan', 'trim|required|max_length[20]', 
+			array('required' => '%s Tidak boleh kosong', 'max_length' => 'Panjang maksimal %s 20 karakter'));
+
+		if ($this->form_validation->run() == FALSE) 
+		{
+			$_SESSION['ResponTitle']  = 'Gagal Simpan!';
+			$_SESSION['ResponMesage'] = validation_errors();
+			$_SESSION['ResponColor']  = 'danger';			
+			$this->session->mark_as_flash(array('ResponMesage', 'ResponColor', 'ResponTitle'));
+			redirect(base_url('master/data_pekerjaan'));
+		} 
+		else 
+		{
+			$response = $this->master_pekerjaan_model->insert_master_pekerjaan(strtoupper($this->input->post('nama_pekerjaan')));
+
+			$_SESSION['ResponTitle']  = $response['return_title'];
+			$_SESSION['ResponMesage'] = $response['return_mesage'];
+			$_SESSION['ResponColor']  = $response['return_color'];			
+			$this->session->mark_as_flash(array('ResponMesage', 'ResponColor', 'ResponTitle'));
+			redirect(base_url('master/data_pekerjaan'));
+		}
+	}
+	/* end master data pekerjaan */
 }
 
 /* End of file Dashboard.php */
---
+++ application/modules/master/models/Master_pekerjaan_model.php
@@ -0,0 +1,67 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Master_pekerjaan_model extends CI_Model {
+
+	public function select_master_pekerjaan()
+	{
+		$query = $this->db->get('master_pekerjaan');
+
+		return $query->result();
+	}
+
+	public function insert_master_pekerjaan($nama_pekerjaan)
+	{
+		$object = array(
+			'nama_pekerjaan' => $nama_pekerjaan, 
+		);
+
+		$this->db->where('nama_pekerjaan', $nama_pekerjaan);
+		$cek = $this->db->get('master_pekerjaan');
+
+		if ($cek->num_rows() > 0) 
+		{
+			$returnData = array(
+				'status' => FALSE,
+				'return_title' => 'Gagal Simpan!',
+				'return_mesage' => 'Sudah ada data "Nama Pekerjaan" yang sama persis dengan yang anda input',
+				'return_color' => 'danger'
+			);
+		}
+		else
+		{
+			try 
+			{
+				$query = $this->db->insert('master_pekerjaan', $object);
+				
+				if (!$query) 
+				{
+					throw new Exception($this->db->_error_message());				
+				}
+
+				$returnData = array(
+					'status' => TRUE,
+					'return_title' => 'Berhasil',
+					'return_mesage' => 'Data Pekerjaan Berhasil Ditambah',
+					'return_color' => 'success'
+				);
+			} 
+			catch (Exception $e) 
+			{
+				$returnData = array(
+					'status' => FALSE,
+					'return_title' => 'Gagal Simpan!',
+					'return_mesage' => 'Terjadi kesalahan saat memproses data',
+					'return_color' => 'danger'
+				);
+			}
+				
+		}
+
+		return $returnData;
+	}
+
+}
+
+/* End of file Master_pekerjaan_model.php */
+/* Location: ./application/modules/master/models/Master_pekerjaan_model.php */
